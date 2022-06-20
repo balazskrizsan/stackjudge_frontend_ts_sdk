4500dc91a696f38b2177b106c83fa57279a49e47
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-06-20 21:47:23 +0200

    Interface response with Observable

--- src/stackjudge_frontend_ts_sdk/schema_interfaces/IAccountGetByReviewIdByReviewId.ts
+++ src/stackjudge_frontend_ts_sdk/schema_interfaces/IAccountGetByReviewIdByReviewId.ts
@@ -1,6 +1,7 @@
 import {IOpenSdkGetable} from "../../common/intercaes/IOpenSdkGetable";
 import {StdResponse} from "../../common/entities/StdResponse";
 import {User} from "../schema_parameter_objects/User";
+import {Observable} from "rxjs";
 
 export const API_URL = "/account/get-by-review-id/{reviewId}"; 
 
@@ -9,5 +10,5 @@ export const API_URL = "/account/get-by-review-id/{reviewId}";
  */
 export interface IAccountGetByReviewIdByReviewId
 {
-    get(getByReviewIdRequest: IOpenSdkGetable): StdResponse<User>;
+    get(getByReviewIdRequest: IOpenSdkGetable): Observable<StdResponse<User>>;
 }
